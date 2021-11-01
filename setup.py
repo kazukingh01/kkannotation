51417aa727fee558dc4aa17ad7f518d474bd9dbf
kazunoki <kazukingh01@gmail.com>
2021-11-01 16:13:30 +0900

    modify

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='kkannotation',
-    version='1.0.1',
+    version='1.0.2',
     description='my annotation library.',
     long_description=long_description,
     long_description_content_type="text/markdown",
