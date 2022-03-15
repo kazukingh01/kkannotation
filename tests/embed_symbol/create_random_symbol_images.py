562456a35a37614d8d82558436c0425be3b9c0dc
kazunoki <kazukingh01@gmail.com>
2022-03-15 09:25:34 +0900

    modify

--- tests/embed_symbol/create_random_symbol_images.py
+++ tests/embed_symbol/create_random_symbol_images.py
@@ -5,9 +5,8 @@ from kkannotation.symbolemb import SymbolEmbedding
 
 
 if __name__ == "__main__":
-    with open("config.json") as f: config = json.load(f)
+    with open("config1.json") as f: config = json.load(f)
     emb = SymbolEmbedding(**config)
-    img = emb.create_image()
+    img, coco = emb.create_image("./test.png", is_save=True)
     cv2.imshow("test", img)
     cv2.waitKey(0)
-
